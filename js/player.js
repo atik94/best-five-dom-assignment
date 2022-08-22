decb4988cd57fea198b45a20e16a2fef8fbc69d0
atik94 <atikurrahmanatik94@gmail.com>
2022-08-22 22:12:26 +0600

    player expense commit

--- js/player.js
+++ js/player.js
@@ -17,9 +17,43 @@ function displayPlayer(playerArrayName) {
   for (let i = 0; i < playerArrayName.length; i++) {
     //console.log(playerArray[i].playerName);
     let name = playerArray[i].playerName;
-    console.log(name);
+    //console.log(name);
     let li = document.createElement("li");
     li.innerText = name;
     container.appendChild(li);
   }
 }
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  if (playerArray.length > 5) {
+    alert("too large");
+  } else {
+    let totalPlayer = playerArray.length;
+    let playerCost = document.getElementById("player-cost");
+    let playerCostString = playerCost.value;
+    let perPlayerCost = parseFloat(playerCostString);
+    let perPlayerCostTotal = totalPlayer * perPlayerCost;
+    let playerExpense = document.getElementById("player-expense");
+    playerExpense.innerText = perPlayerCostTotal;
+  }
+  // let totalPlayer = playerArray.length;
+  // //console.log(totalPlayer);
+  // let playerCost = document.getElementById("player-cost");
+  // let playerCostString = playerCost.value;
+  // let perPlayerCost = parseFloat(playerCostString);
+  // let perPlayerCostTotal = totalPlayer * perPlayerCost;
+  // let playerExpense = document.getElementById("player-expense");
+  // playerExpense.innerText = perPlayerCostTotal;
+});
+
+document.getElementById("calculate-total").addEventListener("click", function () {
+  let managerCost = document.getElementById("manager-cost");
+  let managerCostString = managerCost.value;
+  let managerCostTotal = parseFloat(managerCostString);
+
+  let coachCost = document.getElementById("coach-cost");
+  let coachCostString = coachCost.value;
+  let coachCostTotal = parseFloat(coachCostString);
+  let calculateTotal = managerCostTotal + coachCostTotal;
+  //console.log(playerExpense);
+});
