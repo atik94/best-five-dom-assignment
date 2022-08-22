16266c9c3c4daade8c47fd60efecc662cd8c63cd
atik94 <atikurrahmanatik94@gmail.com>
2022-08-23 01:50:14 +0600

    calculate total commit

--- js/player.js
+++ js/player.js
@@ -45,8 +45,13 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   // let playerExpense = document.getElementById("player-expense");
   // playerExpense.innerText = perPlayerCostTotal;
 });
-
 document.getElementById("calculate-total").addEventListener("click", function () {
+  let totalPlayer = playerArray.length;
+  let playerCost = document.getElementById("player-cost");
+  let playerCostString = playerCost.value;
+  let perPlayerCost = parseFloat(playerCostString);
+  let perPlayerCostTotal = totalPlayer * perPlayerCost;
+
   let managerCost = document.getElementById("manager-cost");
   let managerCostString = managerCost.value;
   let managerCostTotal = parseFloat(managerCostString);
@@ -54,6 +59,8 @@ document.getElementById("calculate-total").addEventListener("click", function ()
   let coachCost = document.getElementById("coach-cost");
   let coachCostString = coachCost.value;
   let coachCostTotal = parseFloat(coachCostString);
-  let calculateTotal = managerCostTotal + coachCostTotal;
-  //console.log(playerExpense);
+  let calculateTotal = managerCostTotal + coachCostTotal + perPlayerCostTotal;
+  let total = document.getElementById("total");
+  total.innerText = calculateTotal;
+  //console.log(calculateTotal);
 });
